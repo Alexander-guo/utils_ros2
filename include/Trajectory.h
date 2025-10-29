96c559fdf0d36b4f3e0946afa7243d37f10bde59
Alex Guo <458467864@qq.com>
2025-10-28 20:46:34 -0400

    Converted all src files to ROS2, built successfully.

--- include/Trajectory.h
+++ include/Trajectory.h
@@ -1,25 +1,26 @@
 #include <string>
-#include <geometry_msgs/Pose.h>
+#include <vector>
 #include <unordered_map>
 #include <set>
+#include "geometry_msgs/msg/pose.hpp"
 
 class Trajectory
 {
     public:
-        Trajectory(std::string &traj_file);
+        Trajectory(const std::string &traj_file);
         ~Trajectory() = default;
 
         bool loadTrajectory(char separator = ' ', bool skip_first_line = false, bool double_stamp = true);
-        bool loadTrajectory(std::string &traj_file, char separator = ' ', bool skip_first_line = false, bool double_stamp = true);
+        bool loadTrajectory(const std::string &traj_file, char separator = ' ', bool skip_first_line = false, bool double_stamp = true);
 
-        bool getPose(const std::uint64_t &timestamp, geometry_msgs::Pose &pose);
-        
+        bool getPose(const std::uint64_t &timestamp, geometry_msgs::msg::Pose &pose);
+
         std::set<std::uint64_t> getTimestamps();
-        std::vector<geometry_msgs::Pose> getPoses();
+        std::vector<geometry_msgs::msg::Pose> getPoses();
 
     private:
         std::string traj_file_;
         std::unordered_map<std::uint64_t, size_t> stamp_to_indx_map_;
         std::set<std::uint64_t> timestamps_;
-        std::vector<geometry_msgs::Pose> poses_;
+        std::vector<geometry_msgs::msg::Pose> poses_;
 };
